e87ce2dcc36ee0cf8720f4726d69f197ab90100f
MikePerez2022 <123500465+MikePerez2022@users.noreply.github.com>
2023-12-07 23:01:48 -0700

    Finished ray tracer final scene

--- Source/RayTracer/Main.cpp
+++ Source/RayTracer/Main.cpp
@@ -19,7 +19,7 @@ int main(int argc, char* argv[])
 
 	const int width = 400;
 	const int height = 300;
-	const int samples = 10;
+	const int samples = 300;
 	const int depth = 8;
 
 	Renderer renderer;
@@ -28,71 +28,51 @@ int main(int argc, char* argv[])
 	Canvas canvas(width, height, renderer);
 
 	float aspectRatio = canvas.GetSize().x / (float)canvas.GetSize().y;
-	//std::shared_ptr<Camera> camera = std::make_shared<Camera>(glm::vec3{ 0, 2, 10 }, glm::vec3{ 0, 1, 0 }, glm::vec3{ 0, 1, 0 }, 20.0f, aspectRatio);
-	std::shared_ptr<Camera> camera = std::make_shared<Camera>(glm::vec3{ 0, 1, 10 }, glm::vec3{ 0, 0, 0 }, glm::vec3{ 0, 1, 0 }, 25.0f, aspectRatio);
+	//std::shared_ptr<Camera> camera = std::make_shared<Camera>(glm::vec3{ 0, 1, 10 }, glm::vec3{ 0, 1, 0 }, glm::vec3{ 0, 1, 0 }, 20.0f, aspectRatio);
+	std::shared_ptr<Camera> camera = std::make_shared<Camera>(glm::vec3{ 0, 2.5f, 10 }, glm::vec3{ 0, 0, 0 }, glm::vec3{ 0, 1, 0 }, 25.0f, aspectRatio);
 
 	Scene scene(glm::vec3{ 1.0f }, glm::vec3{ 0.5f, 0.7f, 1.0f });
 	scene.SetCamera(camera);
 
 	// create material
-	//for (int x = -10; x < 10; x++)
-	//{
-	//	for (int z = -10; z < 10; z++)
-	//	{
-	//
-	//		std::shared_ptr<Material> material;
-	//
-	//		// create random material
-	//		float r = random01();
-	//		if (r < 0.3f)		material = std::make_shared<Lambertian>(glm::rgbColor(glm::vec3{ random(0, 360), 1.0f, 1.0f }));
-	//		else if (r < 0.6f)	material = std::make_shared<Metal>(color3_t{ random(0.5f, 1.0f) }, random(0, 0.5f));
-	//		else if (r < 0.9f)	material = std::make_shared<Dielectric>(color3_t{ 1.0f }, random(1.1f, 2));
-	//		else				material = std::make_shared<Emissive>(glm::rgbColor(glm::vec3{ random(0, 360), 1.0f, 1.0f }), 5.0f);
-	//
-	//		// set random sphere radius
-	//		float radius = random(0.2f, 0.3f);
-	//		// create sphere using random radius and material
-	//		auto sphere = std::make_unique<Sphere>(glm::vec3{ x + random(-0.5f, 0.5f), radius, z + random(-0.5f, 0.5f)}, radius, material);
-	//		// add sphere to the scene
-	//		scene.AddObject(std::move(sphere));
-	//	}
-	//}
+	for (int x = -10; x < 10; x++)
+	{
+		for (int z = -10; z < 10; z++)
+		{
+	
+			std::shared_ptr<Material> material;
+	
+			// create random material
+			float r = random01();
+			if (r < 0.3f)		material = std::make_shared<Lambertian>(glm::rgbColor(glm::vec3{ random(0, 360), 1.0f, 1.0f }));
+			else if (r < 0.6f)	material = std::make_shared<Metal>(color3_t{ random(0.5f, 1.0f) }, random(0, 0.5f));
+			else if (r < 0.9f)	material = std::make_shared<Dielectric>(color3_t{ 1.0f }, random(1.1f, 2));
+			else				material = std::make_shared<Emissive>(glm::rgbColor(glm::vec3{ random(0, 360), 1.0f, 1.0f }), 5.0f);
+	
+			// set random sphere radius
+			float radius = random(0.2f, 0.3f);
+			// create sphere using random radius and material
+			auto sphere = std::make_unique<Sphere>(glm::vec3{ x + random(-0.5f, 0.5f), radius, z + random(-0.5f, 0.5f)}, radius, material);
+			// add sphere to the scene
+			scene.AddObject(std::move(sphere));
+		}
+	}
 
-	auto mesh = std::make_unique<Mesh>(std::make_shared<Emissive>(color3_t{ 1, 1, 1 }, 8.0f));
-	mesh->Load("Models/quad.obj", glm::vec3{ 0, 1.4f, 5.5f }, glm::vec3{ 85, 0, 0 }, glm::vec3{ 0.4f, 0.4f, 0.4f });
+	auto triangle = std::make_unique<Triangle>(glm::vec3{ -1, 0, -1.5f }, glm::vec3{ 1, 0, -1.5f }, glm::vec3{ 0, 2, -1.5f }, std::make_shared<Lambertian>(color3_t{ 1, 0, 0 }));
+	scene.AddObject(std::move(triangle));
+
+	//add meshes
+	auto mesh = std::make_unique<Mesh>(std::make_shared<Lambertian>(color3_t{ 0,1,0 }));
+	mesh->Load("Models/cube.obj", glm::vec3{ -2, 0.5f, 0 }, glm::vec3{ 0, 0, 0 });
+	scene.AddObject(std::move(mesh)); 
+
+	mesh = std::make_unique<Mesh>(std::make_shared<Lambertian>(color3_t{ 0,0,1 }));
+	mesh->Load("Models/cube.obj", glm::vec3{ 2, 0.5f, -3 }, glm::vec3{ 0, 0, 0 }, glm::vec3{1,2,1});
 	scene.AddObject(std::move(mesh));
 
-	mesh = std::make_unique<Mesh>(std::make_shared<Lambertian>(color3_t{ 1, 1, 1 }));
-	mesh->Load("Models/quad.obj", glm::vec3{ 0, 1.4f, 5 }, glm::vec3{ 85, 0, 0 }, glm::vec3{6,5,1});
-	scene.AddObject(std::move(mesh));
-
-	mesh = std::make_unique<Mesh>(std::make_shared<Lambertian>(color3_t{ 1, 1, 1 }));
-	mesh->Load("Models/quad.obj", glm::vec3{ 0, 0.5f, 4 }, glm::vec3{ 0, 0, 0 }, glm::vec3{ 5, 2.1f, 1 });
-	scene.AddObject(std::move(mesh));
-
-	mesh = std::make_unique<Mesh>(std::make_shared<Lambertian>(color3_t{ 1, 1, 1 }));
-	mesh->Load("Models/quad.obj", glm::vec3{ 0, 0.5f, 12 }, glm::vec3{ 0, 0, 0 }, glm::vec3{ 5, 2.1f, 1 });
-	scene.AddObject(std::move(mesh));
-
-	mesh = std::make_unique<Mesh>(std::make_shared<Lambertian>(color3_t{ 1, 0, 0 }));
-	mesh->Load("Models/quad.obj", glm::vec3{ -1.0f, 0.5f, 5 }, glm::vec3{ 0, 90, 0 }, glm::vec3{ 7, 3.1f, 1 });
-	scene.AddObject(std::move(mesh));
-
-	mesh = std::make_unique<Mesh>(std::make_shared<Lambertian>(color3_t{ 0, 1, 0 }));
-	mesh->Load("Models/quad.obj", glm::vec3{ 1.0f, 0.5f, 5 }, glm::vec3{ 0, -90, 0 }, glm::vec3{ 7, 3.1f, 1 });
-	scene.AddObject(std::move(mesh));
-
-	mesh = std::make_unique<Mesh>(std::make_shared<Lambertian>(color3_t{ 1, 1, 1 }));
-	mesh->Load("Models/quad.obj", glm::vec3{ 0, -0.4f, 5 }, glm::vec3{ -85, 0, 0 }, glm::vec3{ 6,5,1 });
-	scene.AddObject(std::move(mesh));
-
-	mesh = std::make_unique<Mesh>(std::make_shared<Lambertian>(color3_t{ 1 }));
-	mesh->Load("Models/cube.obj", glm::vec3{ -0.4f, -0.1f, 5 }, glm::vec3{ 0, 45, 0 }, glm::vec3{ 0.6f ,0.6f ,0.6f });
-	scene.AddObject(std::move(mesh));
-
-	auto material = std::make_shared<Lambertian>(color3_t{ 1, 1, 1 });
-	auto sphere = std::make_unique<Sphere>(glm::vec3{0.4f, -0.1f, 6 }, 0.3f, material);
-	scene.AddObject(std::move(sphere));
+	auto materialPlane = std::make_shared<Lambertian>(color3_t{ 0.2f });
+	auto plane = std::make_unique<Plane>(glm::vec3{ 0, 0, 0 }, glm::vec3{ 0, 1, 0 }, materialPlane);
+	scene.AddObject(std::move(plane));
 
 	seedRandom((glm::uint)time(nullptr));
 
